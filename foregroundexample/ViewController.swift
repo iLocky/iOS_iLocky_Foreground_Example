abfc3afe6b514cbf1d03b2f1178f014b0e356662
Richard Chin <richjing@gmail.com>
2016-01-07 00:09:30 +0800

    Initial Commit

---
+++ foregroundexample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  foregroundexample
+//
+//  Created by Richard on 2016/1/7.
+//  Copyright © 2016年 Richard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
